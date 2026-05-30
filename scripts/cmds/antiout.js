0e233e09843b02ef3b23b323839e5ee8ba2766dc
oneshotemran-coder <oneshotemran@gmail.com>
2026-05-31 00:55:01 +0600

    Update antiout.js

--- scripts/cmds/antiout.js
+++ scripts/cmds/antiout.js
@@ -52,7 +52,7 @@ module.exports = {
 তুই এখন "Escape Legend" 💀🏆
 আর add করা হইবো না! Bye bye 👋
 
-***_Powered by Farhan ⚡_***`,
+***_Powered by 𝗜𝗠𝗥𝗔𝗡 ⚡_***`,
           event.threadID
         );
       }
@@ -82,7 +82,7 @@ Resume তে লিখবি —
 
 আর একবার করলে surprise আছে 💀
 
-***_Powered by Farhan ⚡_***`,
+***_Powered by 𝗜𝗠𝗥𝗔𝗡 ⚡_***`,
               mentions: [{
                 tag: userName,
                 id: userId
@@ -102,7 +102,7 @@ Resume তে লিখবি —
 
 Security তোর উপর নজর রাখতেছে 👀
 
-***_Powered by Farhan ⚡_***`,
+***_Powered by 𝗜𝗠𝗥𝗔𝗡 ⚡_***`,
               mentions: [{
                 tag: userName,
                 id: userId
@@ -122,7 +122,7 @@ Security তোর উপর নজর রাখতেছে 👀
 
 Bot watching you 🕵️‍♂️🔥
 
-***_Powered by Farhan ⚡_***`,
+***_Powered by 𝗜𝗺𝗿𝗮𝗻 ⚡_***`,
               mentions: [{
                 tag: userName,
                 id: userId
@@ -161,7 +161,7 @@ Mission: Re-Added Successfully ✅🔥
           `🤡 পালানোর প্ল্যান সফল হইছে মনে হয়ছে! হয়তো ওর লগে আমি এড না অথবা আমাকে block করছে 😭
 এইবার বেঁচে গেলি 😏
 
-***_Powered by Farhan ⚡_***`,
+***_Powered by 𝗜𝗺𝗿𝗮𝗻 ⚡_***`,
           event.threadID
         );
 
